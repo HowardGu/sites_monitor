0cd42b1151a75a35eb6f68721d2c14d12c70b568
HowardGu <ghbluewater@gmail.com>
2021-05-26 09:47:13 +0100

    Work on SiteInfo page
    1. Add site controlling dialog

--- src/components/SiteInfo.vue
+++ src/components/SiteInfo.vue
@@ -34,7 +34,7 @@
 
                 <el-row :gutter="20" class="siteInfo-row">
                     <el-col :span="6"><el-button @click="getSiteInfo(siteId)">读取信息</el-button></el-col>
-                    <el-col :span="6"><el-button>站点控制</el-button></el-col>
+                    <el-col :span="6"><el-button @click="siteControllingDialogVisible = true">站点控制</el-button></el-col>
                     <el-col :span="6"><el-button>站点设置</el-button></el-col>
                 </el-row>
             </el-card>
@@ -71,6 +71,75 @@
                 </div>
             </el-card>
         </el-card>
+
+        <el-dialog title="站点控制" :visible.sync="siteControllingDialogVisible" width="50%">
+            <el-card class="siteInfo-inner-card">
+                <div slot="header" class="siteInfo-header">
+                    <span>设备信息</span>
+                    <el-button type="primary">重启设备</el-button>
+                </div>
+
+                <el-row :gutter="20" class="siteInfo-row">
+                    <el-col :span="4"><span>站点号(1 ~ 99999)：</span></el-col>
+                    <el-col :span="12"><el-input-number v-model="siteId" controls-position="right" :min="1" :max="99999"></el-input-number></el-col>
+                    <el-col :span="6"><el-button>设置</el-button></el-col>
+                </el-row>
+
+                <el-row :gutter="20" class="siteInfo-row">
+                    <el-col :span="4">
+                        <span>时钟：</span>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-date-picker v-model="siteControllingDate.dateTime" type="datetime" placeholder="选择日期时间" :default-time="['12:00:00']"></el-date-picker>
+                    </el-col>
+                    <el-col :span="6"><el-button>设置</el-button></el-col>
+                </el-row>
+
+                <el-row :gutter="20" class="siteInfo-row">
+                    <el-col :span="4">
+                        <span>服务器：</span>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-input v-model="siteControllingDate.serverAddress" placeholder="服务器地址"></el-input>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-input v-model="siteControllingDate.serverPort" placeholder="服务器端口"></el-input>
+                    </el-col>
+                    <el-col :span="6"><el-button>设置</el-button></el-col>
+                </el-row>
+            </el-card>
+
+            <el-card class="siteInfo-inner-card">
+                <div slot="header" class="siteInfo-header">
+                    <span>设备控制</span>
+                </div>
+
+                <el-row :gutter="20" class="siteInfo-row">
+                    <el-col :span="4"><span>开关状态：</span></el-col>
+                    <el-col :span="6">
+                        <el-select v-model="siteControllingDate.state" placeholder="请选择开关状态">
+                            <el-option label="关机" :value="false"></el-option>
+                            <el-option label="开机" :value="true"></el-option>
+                        </el-select>
+                    </el-col>
+                    <el-col :span="6"><el-button>设置</el-button></el-col>
+                </el-row>
+
+                <el-row :gutter="20" class="siteInfo-row">
+                    <el-col :span="4">
+                        <span>额定功率(W)：</span>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-input v-model="siteControllingDate.ratedPower"></el-input>
+                    </el-col>
+                    <el-col :span="6"><el-button>设置</el-button></el-col>
+                </el-row>
+            </el-card>
+
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="siteControllingDialogVisible = false">关闭</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -78,7 +147,17 @@
 export default {
     data() {
         return {
-            siteId: 1
+            siteId: 1,
+
+            siteControllingDialogVisible: false,
+
+            siteControllingDate: {
+                dateTime: '',
+                serverAddress: '',
+                serverPort: '',
+                state: false,
+                ratedPower: 0
+            }
         };
     },
     methods: {
