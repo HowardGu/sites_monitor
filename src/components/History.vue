4747bf5ae1bf6fd561f9fdaa2f58c682212a2043
HowardGu <ghbluewater@gmail.com>
2021-06-02 16:48:11 +0100

    Work on History page
    1. Put totally five charts in this page
    2. Use the real api call for getSiteHistory

--- src/components/History.vue
+++ src/components/History.vue
@@ -16,13 +16,16 @@
 
             <div class="history-search-bar">
                 <el-input-number v-model="siteId" controls-position="right" :min="1" :max="99999"></el-input-number>
-                <el-date-picker v-model="dateTimeRange" type="datetimerange"  class="history-search-bar-datepicker"
+                <el-date-picker v-model="dateTimeRange" type="datetimerange"  class="history-search-bar-datepicker" :unlink-panels="true"
                     start-placeholder="开始时间" end-placeholder="结束时间" :default-time="['12:00:00']"></el-date-picker>
-                <el-button class="history-search-bar-button" icon="el-icon-search">刷新曲线</el-button>
+                <el-button class="history-search-bar-button" icon="el-icon-search" @click="getSiteHistory(siteId)">刷新曲线</el-button>
             </div>
 
             <div id="incidentPowerChart" style="margin-top: 50px; width: 100%; height:400px;"></div>
             <div id="reflectedPowerChart" style="margin-top: 50px; width: 100%; height:400px;"></div>
+            <div id="pushPowerChart" style="margin-top: 50px; width: 100%; height:400px;"></div>
+            <div id="electricCurrentChart" style="margin-top: 50px; width: 100%; height:400px;"></div>
+            <div id="temperatureChart" style="margin-top: 50px; width: 100%; height:400px;"></div>
         </el-card>
     </div>
 </template>
@@ -34,84 +37,169 @@ import CsvExportor from 'csv-exportor'
 export default {
     data() {
         return {
-            siteId: 1,
+            siteId: 2,
 
             dateTimeRange: '',
 
-            powerList: [
-                { dateTime: '2000-06-05 13:05:13', incidentPower: 116, reflectedPower: 55 },
-                { dateTime: '2000-06-06 14:05:13', incidentPower: 45, reflectedPower: 111 },
-                { dateTime: '2000-06-07 15:05:13', incidentPower: 99, reflectedPower: 13 },
-                { dateTime: '2000-06-08 16:05:13', incidentPower: 234, reflectedPower: 54 }
-            ]
+            queryInfo: {
+                startTime: '',
+                endTime: ''
+            },
+
+            logList: [
+            ],
+
+            incidentPowerChart: null,
+
+            reflectedPowerChart: null,
+
+            pushPowerChart: null,
+
+            electricCurrentChart: null,
+
+            temperatureChart: null
         };
     },
     methods: {
         exportToCSV() {
             const header = ['时间', '入射功率', '反射功率'];
-            CsvExportor.downloadCsv(this.powerList, { header }, 'history.csv');
+            CsvExportor.downloadCsv(this.logList, { header }, 'history.csv');
+        },
+
+        async getSiteHistory(siteId) {
+            this.queryInfo.startTime = (new Date(this.dateTimeRange[0])).toISOString();
+            this.queryInfo.endTime = (new Date(this.dateTimeRange[1])).toISOString();
+            try {
+                const tokenStr = window.sessionStorage.getItem('token');
+                const result = await this.$http.post(`history/sites/${siteId}`, this.queryInfo, { headers: { Authorization: `Bearer ${tokenStr}` } });
+                console.log(result);
+
+                if (result.data.code === 200) {
+                    if (result.data.data.logs) {
+                        this.logList = result.data.data.logs;
+                        this.renderCharts();
+                    } else {
+                        this.resetCharts();
+                        this.$message('站点' + this.siteId + '在此时间段内没有日志');
+                    }
+                } else {
+                    this.$message.error(result.data.msg);
+                }
+            } catch (err) {
+                 return this.$message.error(err.response.data.msg);
+            }
+        },
+
+        renderCharts() {
+            const dateList = this.logList.map((item) => {
+                return item.dateTime;
+            });
+
+            const incidentPowerValueList = this.logList.map((item) => {
+                return item.incidentPower;
+            });
+
+            const reflectedPowerValueList = this.logList.map((item) => {
+                return item.reflectedPower;
+            });
+
+            const pushPowerValueList = this.logList.map((item) => {
+                return item.pushPower;
+            });
+
+            const electricCurrentList = this.logList.map((item) => {
+                return item.electricCurrent;
+            });
+
+            const temperatureList = this.logList.map((item) => {
+                return item.temperature;
+            });
+
+            const commonChartOption = {
+                tooltip: {
+                    trigger: 'axis'
+                },
+                xAxis: {
+                    type: 'category',
+                    data: dateList
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [{
+                    type: 'line'
+                }]
+            };
+
+            this.incidentPowerChart.clear();
+            this.incidentPowerChart.setOption(commonChartOption);
+            this.incidentPowerChart.setOption({ title: { text: '入射功率' }, series: [{ data: incidentPowerValueList }] });
+
+            this.reflectedPowerChart.clear();
+            this.reflectedPowerChart.setOption(commonChartOption);
+            this.reflectedPowerChart.setOption({ title: { text: '反射功率' }, series: [{ data: reflectedPowerValueList }] });
+
+            this.pushPowerChart.clear();
+            this.pushPowerChart.setOption(commonChartOption);
+            this.pushPowerChart.setOption({ title: { text: '推动功率' }, series: [{ data: pushPowerValueList }] });
+
+            this.electricCurrentChart.clear();
+            this.electricCurrentChart.setOption(commonChartOption);
+            this.electricCurrentChart.setOption({ title: { text: '功放电流' }, series: [{ data: electricCurrentList }] });
+
+            this.temperatureChart.clear();
+            this.temperatureChart.setOption(commonChartOption);
+            this.temperatureChart.setOption({ title: { text: '功放温度' }, series: [{ data: temperatureList }] });
+        },
+
+        resetCharts() {
+            const commonChartOption = {
+                tooltip: {
+                    trigger: 'axis'
+                },
+                xAxis: {
+                    type: 'category'
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [{
+                    type: 'line'
+                }]
+            };
+
+            this.incidentPowerChart.clear();
+            this.incidentPowerChart.setOption(commonChartOption);
+            this.incidentPowerChart.setOption({ title: { text: '入射功率' } });
+
+            this.reflectedPowerChart.clear();
+            this.reflectedPowerChart.setOption(commonChartOption);
+            this.reflectedPowerChart.setOption({ title: { text: '反射功率' } });
+
+            this.pushPowerChart.clear();
+            this.pushPowerChart.setOption(commonChartOption);
+            this.pushPowerChart.setOption({ title: { text: '推动功率' } });
+
+            this.electricCurrentChart.clear();
+            this.electricCurrentChart.setOption(commonChartOption);
+            this.electricCurrentChart.setOption({ title: { text: '功放电流' } });
+
+            this.temperatureChart.clear();
+            this.temperatureChart.setOption(commonChartOption);
+            this.temperatureChart.setOption({ title: { text: '功放温度' } });
+        },
+
+        initCharts() {
+            this.incidentPowerChart = echarts.init(document.getElementById('incidentPowerChart'));
+            this.reflectedPowerChart = echarts.init(document.getElementById('reflectedPowerChart'));
+            this.pushPowerChart = echarts.init(document.getElementById('pushPowerChart'));
+            this.electricCurrentChart = echarts.init(document.getElementById('electricCurrentChart'));
+            this.temperatureChart = echarts.init(document.getElementById('temperatureChart'));
         }
     },
     mounted() {
-        const dateList = this.powerList.map((item) => {
-            return item.dateTime;
-        });
-
-        const incidentPowerValueList = this.powerList.map((item) => {
-            return item.incidentPower;
-        });
-
-        const reflectedPowerValueList = this.powerList.map((item) => {
-            return item.reflectedPower;
-        });
-
-        const incidentPowerChart = echarts.init(document.getElementById('incidentPowerChart'));
-
-        const incidentPowerOption = {
-            title: {
-                text: '入射功率'
-            },
-            tooltip: {
-                trigger: 'axis'
-            },
-            xAxis: {
-                type: 'category',
-                data: dateList
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [{
-                data: incidentPowerValueList,
-                type: 'line'
-            }]
-        };
-
-        incidentPowerChart.setOption(incidentPowerOption);
-
-        const reflectedPowerChart = echarts.init(document.getElementById('reflectedPowerChart'));
-
-        const reflectedPowerOption = {
-            title: {
-                text: '反射功率'
-            },
-            tooltip: {
-                trigger: 'axis'
-            },
-            xAxis: {
-                type: 'category',
-                data: dateList
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [{
-                data: reflectedPowerValueList,
-                type: 'line'
-            }]
-        };
-
-        reflectedPowerChart.setOption(reflectedPowerOption);
+        this.initCharts();
+        this.resetCharts();
     }
 }
 </script>
