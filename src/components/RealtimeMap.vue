6bb08f87ef3c6546343511f282ca4d710c9e179e
HowardGu <ghbluewater@gmail.com>
2021-06-07 07:26:33 +0100

    Add Baidu map for RealtimeMap page
    1. Add baidu map
    2. Support show marker of site in map
    3. Support show window when clicking the marker

--- src/components/RealtimeMap.vue
+++ src/components/RealtimeMap.vue
@@ -9,13 +9,73 @@
             <div slot="header" class="realtimeMap-card-header">
                 <span>实时地图</span>
             </div>
+
+            <baidu-map :style="{width:map.width,height:map.height}" class="map" :scroll-wheel-zoom="true"
+                center="厦门" @ready="mapHandler" ak="ak">
+                <bm-scale anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-scale>
+
+                <bm-navigation anchor="BMAP_ANCHOR_BOTTOM_RIGHT" ></bm-navigation>
+
+                <bm-marker v-for="marker of markers" :key="marker.siteId" :position="{lng: marker.longitude, lat: marker.latitude}" @click="openInfoWindow(marker)">
+                    <bm-info-window :show="showInfoWindow" @close="closeInfoWindow"></bm-info-window>
+                </bm-marker>
+            </baidu-map>
         </el-card>
     </div>
 </template>
 
 <script>
+import BaiduMap from 'vue-baidu-map/components/map/Map.vue'
+import BmScale from 'vue-baidu-map/components/controls/Scale'
+import BmNavigation from 'vue-baidu-map/components/controls/Navigation'
+import BmMarker from 'vue-baidu-map/components/overlays/Marker'
+import BmInfoWindow from 'vue-baidu-map/components/overlays/InfoWindow'
 export default {
+    components: {
+        BaiduMap,
+        BmScale,
+        BmNavigation,
+        BmMarker,
+        BmInfoWindow
+    },
+    data() {
+        return {
+            ak: this.$baiduMapAK,
 
+            map: {
+                width: '100%',
+                height: '600px'
+            },
+
+            markers: [
+                {
+                    siteId: 1,
+                    longitude: 118.1095150000,
+                    latitude: 24.5019670000,
+                    tunnel: '仙岳路隧道',
+                    location: '横洞',
+                    siteName: 'FM远端机1',
+                    description: 'test site 1'
+                }
+            ],
+
+            showInfoWindow: false
+        };
+    },
+    methods: {
+        mapHandler({ BMap, map }) {
+            this.map.height = document.body.clientHeight - 160 + 'px';
+        },
+
+        openInfoWindow(marker) {
+            this.showInfoWindow = true;
+            console.log(marker);
+        },
+
+        closeInfoWindow() {
+            this.showInfoWindow = false;
+        }
+    }
 }
 </script>
 
@@ -24,5 +84,10 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
+}
+
+.map {
+    width: 100%;
+    height: 600px;
 }
 </style>
