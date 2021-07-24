6257ebb78b3945d334fcd38c9b6e9b8e6a99bf2f
BrianWChen <brian.c@163.com>
2021-07-24 09:32:44 +0100

    Remove unused API

--- src/service/logService.js
+++ src/service/logService.js
@@ -1,13 +1,5 @@
 import request from '@/utils/request';
 
-const show = (siteUUID, axiosParams) => {
-    return request.get(`logs/sites/${siteUUID}`, { params: axiosParams });
-};
-
-const showAll = (axiosParams) => {
-    return request.get('logs', { params: axiosParams });
-};
-
 const showRealtimeLog = (siteUUID) => {
     return request.get(`realtime/sites/${siteUUID}`);
 };
@@ -21,8 +13,6 @@ const showHistory = (siteUUID, historyParams) => {
 };
 
 export default {
-    show,
-    showAll,
     showRealtimeLog,
     showRealtimeData,
     showHistory
