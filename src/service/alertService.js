40099a4036da08f37ada697f930144a224dbf459
BrianWChen <brian.c@163.com>
2021-08-01 12:45:35 +0100

    Fix axios params bugs

--- src/service/alertService.js
+++ src/service/alertService.js
@@ -1,11 +1,11 @@
 import request from '@/utils/request';
 
 const show = (siteUUID, axiosParams, historyParams) => {
-    return request.post(`alerts/sites/${siteUUID}`, { ...historyParams, params: axiosParams });
+    return request.post(`alerts/sites/${siteUUID}`, historyParams, { params: axiosParams });
 };
 
 const showAll = (axiosParams, historyParams) => {
-    return request.post('alerts', { ...historyParams, params: axiosParams });
+    return request.post('alerts', historyParams, { params: axiosParams });
 };
 
 export default {
