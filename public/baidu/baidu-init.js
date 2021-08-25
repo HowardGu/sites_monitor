45f39e63ef766cadfe19460c9a61c6cee02952b4
HowardGu <ghbluewater@gmail.com>
2021-08-25 10:21:38 +0100

    Get dynamic ip of server in map related js

--- public/baidu/baidu-init.js
+++ public/baidu/baidu-init.js
@@ -1,1 +1,16 @@
-(function(){window.HOST_TYPE = "2"; window.BMap_loadScriptTime = (new Date).getTime(); window.BMap=window.BMap||{};window.BMap.apiLoad=function(){delete window.BMap.apiLoad;if(typeof _initBaiduMap=="function"){_initBaiduMap()}};var s=document.createElement('script');s.src='http://localhost:8080/baidu/baidu-api.js';document.body.appendChild(s);})();
+(function() {
+    window.HOST_TYPE = "2";
+    window.BMap_loadScriptTime = (new Date).getTime();
+    window.BMap = window.BMap || {};
+    window.BMap.apiLoad = function() {
+        delete window.BMap.apiLoad;
+        if (typeof _initBaiduMap == "function") {
+            _initBaiduMap()
+        }
+    };
+    var s = document.createElement('script');
+    const hostname = self.location.hostname;
+    console.log('baidu-init.js calls ' + hostname);
+    s.src = 'http://' + hostname + ':8080/baidu/baidu-api.js';
+    document.body.appendChild(s);
+})();
